6af8fdd2b184930753e4186303f282e42221f7cb
asuka431 <hironikki.2170@gmail.com>
2021-07-27 13:41:08 +0900

    yescrypt0.5への対応

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -9,6 +9,7 @@
 #include <primitives/transaction.h>
 #include <serialize.h>
 #include <uint256.h>
+#include <crypto/yescrypt-0.5/yescrypt.c>
 
 /** Nodes collect new transactions into a block, hash them into a hash tree,
  * and scan through nonce values to make the block's hash satisfy proof-of-work
